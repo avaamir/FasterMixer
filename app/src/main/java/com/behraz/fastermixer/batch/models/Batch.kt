3dd0a6efb41a86811e1a326559fdd64c9e0bf517
amirhmp <mp.amir1992@gmail.com>
2020-06-26 17:04:43 +0430

    test connection to git

--- app/src/main/java/com/behraz/fastermixer/batch/models/Batch.kt
+++ app/src/main/java/com/behraz/fastermixer/batch/models/Batch.kt
@@ -2,7 +2,7 @@ package com.behraz.fastermixer.batch.models
 
 data class Batch(
     val id: Int,
-    val name: String,
+    val name: String, //ddd
     val isAvailable: Boolean
 ) {
     val state get() = if (isAvailable) "در دسترس" else "در حال استفاده"
