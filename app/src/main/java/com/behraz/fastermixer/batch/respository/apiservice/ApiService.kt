d318a6e48829f10793f1786de9df998e454f33e7
avaamir <mp.amir1997@gmail.com>
2020-08-19 17:02:23 +0430

    Server IP Changed

--- app/src/main/java/com/behraz/fastermixer/batch/respository/apiservice/ApiService.kt
+++ app/src/main/java/com/behraz/fastermixer/batch/respository/apiservice/ApiService.kt
@@ -22,8 +22,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiService {
-    const val Domain = "http://2.184.49.133:13013"
-    //const val Domain = "http://192.168.1.4:10101"
+    const val Domain = "http://78.39.159.41:8000"
     private const val BASE_API_URL = "$Domain/api/"
 
 
